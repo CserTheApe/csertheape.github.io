3208c6ac88a77e4ef76149c8b00b1fd843289750
CserTheApe <csertheape@gmail.com>
2022-07-17 16:47:10 +1000

    added duolingo, khan academy and updated icons in profiles

--- src/lib/index/profiles.svelte
+++ src/lib/index/profiles.svelte
@@ -2,19 +2,31 @@
 	import Container from '$lib/common/container.svelte';
 	import ProfileCard from '$lib/common/profile-card.svelte';
 	import hackerrankLogo from '$lib/assets/profiles/hackerrank.png';
-	import linkedInLogo from '$lib/assets/profiles/linked-in.png';
-	import salesforceLogo from '$lib/assets/profiles/salesforce.jpg';
+	import linkedLogo from '$lib/assets/profiles/linked.png';
+	import salesforceLogo from '$lib/assets/profiles/salesforce.png';
+	import duolingoLogo from '$lib/assets/profiles/duolingo.png';
+	import khanLogo from '$lib/assets/profiles/khan.png';
 
 	let profiles = [
 		{
 			name: 'LinkedIn',
-			logo: linkedInLogo,
+			logo: linkedLogo,
 			link: 'https://www.linkedin.com/in/josin-abraham-31b3a6118/'
 		},
 		{
 			name: 'Hackerrank',
 			logo: hackerrankLogo,
 			link: 'https://www.hackerrank.com/CserTheApe'
+		},
+		{
+			name: 'Duolingo',
+			logo: duolingoLogo,
+			link: 'https://www.duolingo.com/profile/josin.abraham'
+		},
+		{
+			name: 'Khan Academy',
+			logo: khanLogo,
+			link: 'https://www.khanacademy.org/profile/CserTheApe/'
 		},
 		{
 			name: 'Salesforce',
